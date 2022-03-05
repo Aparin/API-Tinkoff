020dbdd2a3ccc8d71b7b07fee7b2889cf6e28752
Александр Апарин <mir-uspeha@mail.ru>
2022-03-05 18:08:30 +0300

    Адаптация к WP и дизайну сайта

---
+++ znanion_dividends/stocks_info/sberp.php
@@ -0,0 +1,18 @@
+<?php
+
+$sberp = [
+    $figi = "BBG0047315Y7",
+    $title = "Сбербанк ап (тикер: SBERP)",
+    $dividend_amount = 26.36,
+    $currency = "руб",
+    $description = "<p>Сбербанк (переименован в «Сбер») крупнейший банк РФ.  В последние годы развивает направления: брокерские услуги, доставку «Сбер Мега Маркет», «Сбер Еаптека», «Сбер Здоровье», «О2О Холдинг» (совместное предприятие с Mail.ru Group; входят «Ситимобил», Delivery Club, «Кухня на районе», «Самокат», «Ситидрайв», SberFood)  и другие.</p>
+    <p>Может не выплатить дивиденды, или выплатить меньше из-за санкций, однако основную прибыль генерирует в РФ, так что интересен на долгосрок</p>
+    <p><b>Купить</b> привилегированные акции Сбера можно:
+    <a class='attention' href='https://www.finam.ru/quote/moex-akcii/sberbank-pref/?AgencyBackOfficeID=46&agent=22d0a439-78b4-4932-a042-fdc54a8f0b0c&utm_term=sberp' target='_blank' rel='nofollow noopener'>
+    здесь</a>
+    </p>
+    <p><b>Подробнее</b> об акциях Сбербанка
+    <a class='attention' href='https://znanion.ru/dengi/dividendy-i-dohodnost-akcij-sberbanka-obychnye-i-privilegirovannye/' target='_blank' >здесь</a>
+    </p>
+    "
+];
---
+++ znanion_dividends/znanion_dividends.php
@@ -0,0 +1,80 @@
+<?php
+/* Plugin Name: Дивидендные акции | Znanion.ru
+ * Plugin URI:
+ * Author: Апарин Александр
+ * Author URI: https://efeto.ru
+ * Description: Контакты. Email: engineer.aparin@gmail.com, Telegram: https://t.me/aaaparin
+ * Version: 1.0
+ */
+
+if (!defined('ABSPATH')) {
+      exit; // Exit if accessed directly
+}
+
+
+function dividend_stocks()
+{ ?>
+      <style>
+            .card_stock {
+                  border: 1px dotted grey;
+                  margin: 5px 2px;
+                  padding: 0px 10px;
+                  background-color: #6699FF;
+                  color: white;
+                  font-family: Georgia, 'Times New Roman', Times, serif;
+                  font-size: 18px;
+                  box-shadow: inset 1px 1px 6px 0 #dcdcdc;
+            }
+
+            .attention {
+                  background-color: yellow;
+                  color: #0d6efd;
+                  padding: 6px;
+                  border-radius: 50%;
+            }
+
+            h3 {
+                  color: white;
+                  padding-top: 10px;
+            }
+
+            h2 {
+                  color: #49423d;
+            }
+
+            ;
+      </style>
+      <h2>Российские дивидендные акции: ожидаемые дивиденды в 2022</h2>
+
+<?php
+      include 'stocks_array.php';
+      /*************** Вывод карточек           ****************/
+
+      function create_card($stock)
+      {
+            $title = $stock->title;
+            $last_price = $stock->last_price;
+            $time = $stock->time;
+            $dividend_amount = $stock->dividend_amount;
+            $currency = $stock->currency;
+            $description = $stock->description;
+            $dividend_yield = $stock->dividend_yield;
+            echo ("
+            <section class='card_stock'>
+                <h3>$title</h3>
+                <p><b>Ожидаемая дивидендная доходность</b>:  <span class='attention'><b>$dividend_yield%</b></span>.</p> 
+                <p>Цена: $last_price $currency <i>(на $time GMT)</i>. Размер дивидендов: $dividend_amount $currency.</p>
+                $description
+            </section>
+            ");
+      }
+
+      foreach ($sorted_stocks as $stock) {
+            create_card($stock);
+      }
+      /*************** Конец вывода        ****************/
+}
+
+add_shortcode('dividend_stocks', 'dividend_stocks');
+
+?>
