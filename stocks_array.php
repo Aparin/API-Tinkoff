320c13286def68105081565a011c85f11ee200e3
Александр Апарин <mir-uspeha@mail.ru>
2022-03-05 12:03:33 +0300

    Добавил данные Газпрома, почистил код

--- stocks_array.php
+++ stocks_array.php
@@ -45,33 +45,9 @@ function create_stock($stocks_data, $arg)
 }
 include './stocks_info/sberp.php';
 array_push($stocks, create_stock($stocks_data, $sberp));
+include './stocks_info/gazprom.php';
+array_push($stocks, create_stock($stocks_data, $gazprom));
 
-/*
-
-$sber = new Share(
-    $title = "Сбербанк ап",
-    $last_price = $price,
-    $dividend_amount = 24,
-    $currency = "руб",
-    $description = "<p>Префы Сбера могут быть хорошей инвестицией на долгосрок</p>"
-);
-array_push($stocks, $sber);
-
-$price = get_price($arr, "BBG004730RP0");
-$gazprom = new Share(
-    $title = "Газпром (тикер: GAZP)",
-    $last_price = $price,
-    $dividend_amount = 45.00,
-    $currency = "руб",
-    $description = "<p>«Газпром» ожидает рекордные дивиденды по итогам 2021 года, говорится в презентации компании. 
-        За 9 мес. 2021 ожидается дивидендов 29,71 рубля на акцию (50% чистой прибыли по МФСО). 4 квартал ожидается рекордным по прибыли за всю историю компании.</p>
-        <p>Дивиденды выплачивает 1 раз в год. Ожидаемый дивиденд за 2021 (отсечка в июле 2022) — не менее 45 руб. на акцию (заявление руководства).</p>
-        <p>Купить акции Газпрома можно 
-        <a class='attention' href='https://www.finam.ru/quote/moex-akcii/gazprom/?AgencyBackOfficeID=46&amp;agent=22d0a439-78b4-4932-a042-fdc54a8f0b0c&amp;utm_term=gazprom' target='_blank' rel='nofollow noopener'>
-        здесь</a>
-        "
-);
-array_push($stocks, $gazprom);
 /*************** Данные акций Конец ****************/
 
 /*************** Сортировка массива объектов ****************/
---
+++ stocks_info/gazprom.php
@@ -0,0 +1,57 @@
+<?php
+$gazprom = [
+    $figi = "BBG004730RP0",
+    $title = "Газпром (тикер: GAZP)",
+    $dividend_amount = 45.00,
+    $currency = "руб",
+    $description = "<p>«Газпром» ожидает рекордные дивиденды по итогам 2021 года, говорится в презентации компании. 
+    За 9 мес. 2021 ожидается дивидендов 29,71 рубля на акцию (50% чистой прибыли по МФСО). 4 квартал ожидается рекордным по прибыли за всю историю компании.</p>
+    <p>Дивиденды выплачивает 1 раз в год. Ожидаемый дивиденд за 2021 (отсечка в июле 2022) — не менее 45 руб. на акцию (заявление руководства).</p>
+    <table dir='ltr' border='1' cellspacing='0' cellpadding='0'><colgroup> <col width='100' /> <col width='158' /></colgroup>
+<tbody>
+<tr>
+<td style='text-align: center;' colspan='2' rowspan='1' data-sheets-value='{&quot;1&quot;:2,&quot;2&quot;:&quot;Уровни и ожидаемая див. доходность&quot;}'>Уровни и ожидаемая див. доходность</td>
+</tr>
+<tr>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:2,&quot;2&quot;:&quot;Уровень&quot;}'><strong>Уровень</strong></td>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:2,&quot;2&quot;:&quot;доходность&quot;}'><strong>доходность</strong></td>
+</tr>
+<tr>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:328}'>328</td>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:0.13719512195121952}' data-sheets-numberformat='{&quot;1&quot;:3,&quot;2&quot;:&quot;0.00%&quot;,&quot;3&quot;:1}' data-sheets-formula='=R1C[0]/R[0]C[-1]'>13,72%</td>
+</tr>
+<tr>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:280}'>280</td>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:0.16071428571428573}' data-sheets-numberformat='{&quot;1&quot;:3,&quot;2&quot;:&quot;0.00%&quot;,&quot;3&quot;:1}' data-sheets-formula='=R1C[0]/R[0]C[-1]'>16,07%</td>
+</tr>
+<tr>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:270}'>270</td>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:0.16666666666666666}' data-sheets-numberformat='{&quot;1&quot;:3,&quot;2&quot;:&quot;0.00%&quot;,&quot;3&quot;:1}' data-sheets-formula='=R1C[0]/R[0]C[-1]'>16,67%</td>
+</tr>
+<tr>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:232}'>232</td>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:0.1939655172413793}' data-sheets-numberformat='{&quot;1&quot;:3,&quot;2&quot;:&quot;0.00%&quot;,&quot;3&quot;:1}' data-sheets-formula='=R1C[0]/R[0]C[-1]'>19,40%</td>
+</tr>
+<tr>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:223}'>223</td>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:0.20179372197309417}' data-sheets-numberformat='{&quot;1&quot;:3,&quot;2&quot;:&quot;0.00%&quot;,&quot;3&quot;:1}' data-sheets-formula='=R1C[0]/R[0]C[-1]'>20,18%</td>
+</tr>
+<tr>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:213}'>213</td>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:0.2112676056338028}' data-sheets-numberformat='{&quot;1&quot;:3,&quot;2&quot;:&quot;0.00%&quot;,&quot;3&quot;:1}' data-sheets-formula='=R1C[0]/R[0]C[-1]'>21,13%</td>
+</tr>
+<tr>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:206}'>206</td>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:0.21844660194174756}' data-sheets-numberformat='{&quot;1&quot;:3,&quot;2&quot;:&quot;0.00%&quot;,&quot;3&quot;:1}' data-sheets-formula='=R1C[0]/R[0]C[-1]'>21,84%</td>
+</tr>
+<tr>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:155}'>155</td>
+<td style='text-align: center;' data-sheets-value='{&quot;1&quot;:3,&quot;3&quot;:0.2903225806451613}' data-sheets-numberformat='{&quot;1&quot;:3,&quot;2&quot;:&quot;0.00%&quot;,&quot;3&quot;:1}' data-sheets-formula='=R1C[0]/R[0]C[-1]'>29,03%</td>
+</tr>
+</tbody>
+</table>
+    <p>Купить акции Газпрома можно 
+    <a class='attention' href='https://www.finam.ru/quote/moex-akcii/gazprom/?AgencyBackOfficeID=46&amp;agent=22d0a439-78b4-4932-a042-fdc54a8f0b0c&amp;utm_term=gazprom' target='_blank' rel='nofollow noopener'>
+    здесь</a>
+    "
+];
